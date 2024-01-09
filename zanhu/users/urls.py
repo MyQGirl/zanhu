256bae6f93ff2272be768d5b7cdd1563dfdd01f0
sarscov2 <2698892343@qq.com>
2024-01-10 00:37:01 +0800

    5-6 完成用户个人中心开发

--- zanhu/users/urls.py
+++ zanhu/users/urls.py
@@ -9,6 +9,6 @@ from zanhu.users.views import (
 app_name = "users"
 urlpatterns = [
     path("~redirect/", view=user_redirect_view, name="redirect"),
-    path("~update/", view=user_update_view, name="update"),
-    path("<str:username>/", view=user_detail_view, name="detail"),
+    path("update/", view=user_update_view, name="update"),
+    path("<str:username>/", view=user_detail_view, name="detail"),  # 这个username参数也是视图中定义的slug_url_kwarg
 ]
--- zanhu/users/views.py
+++ zanhu/users/views.py
@@ -9,38 +9,32 @@ User = get_user_model()
 
 
 class UserDetailView(LoginRequiredMixin, DetailView):
-
     model = User
     slug_field = "username"
     slug_url_kwarg = "username"
+    template_name = "users/user_detail.html"
 
 
 user_detail_view = UserDetailView.as_view()
 
 
 class UserUpdateView(LoginRequiredMixin, UpdateView):
-
     model = User
-    fields = ["name"]
+    fields = ['nickname', 'email', 'picture', 'introduction', 'job_title', 'location',
+              'personal_url', 'weibo', 'zhihu', 'github', 'linkedin']
+    template_name = "users/user_form.html"
 
     def get_success_url(self):
         return reverse("users:detail", kwargs={"username": self.request.user.username})
 
-    def get_object(self):
-        return User.objects.get(username=self.request.user.username)
-
-    def form_valid(self, form):
-        messages.add_message(
-            self.request, messages.INFO, _("Infos successfully updated")
-        )
-        return super().form_valid(form)
+    def get_object(self, queryset=None):
+        return self.request.user
 
 
 user_update_view = UserUpdateView.as_view()
 
 
 class UserRedirectView(LoginRequiredMixin, RedirectView):
-
     permanent = False
 
     def get_redirect_url(self):
